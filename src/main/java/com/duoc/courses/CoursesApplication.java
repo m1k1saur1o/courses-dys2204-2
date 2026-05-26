38b7e5ae13eb30177818acde340d488a1abe1e5d
m1k1saur1o <miguel.gar.ven@gmail.com>
2026-05-26 00:18:37 -0400

    added comment to test workflow

--- src/main/java/com/duoc/courses/CoursesApplication.java
+++ src/main/java/com/duoc/courses/CoursesApplication.java
@@ -3,6 +3,7 @@ package com.duoc.courses;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//main app
 @SpringBootApplication
 public class CoursesApplication {
 
